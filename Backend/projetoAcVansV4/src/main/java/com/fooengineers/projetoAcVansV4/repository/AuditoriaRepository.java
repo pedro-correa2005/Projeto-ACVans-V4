c951575527f75db1e7b993d5104059ad66baa3c8
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:17:27 -0300

    Create AuditoriaRepository.java
    Repository para auditoria

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/AuditoriaRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.Auditoria;
+
+public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
+
+}

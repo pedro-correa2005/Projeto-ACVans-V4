c7c2f0ce32ee0412e19ff6c84e1180c3df13a7b8
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:03:51 -0300

    Update OficinaRepository.java
    Query para pesquisa por parâmetro

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/OficinaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/OficinaRepository.java
@@ -9,4 +9,7 @@ import org.springframework.data.repository.query.Param;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
 public interface OficinaRepository extends JpaRepository<Oficina, Long>{
+	//Query para pesquisa por parâmetro id ou nome (barra de pesquisa)
+	@Query(value="SELECT o FROM Oficina o WHERE STR(o.id) LIKE :param OR o.nome LIKE CONCAT('%', :param ,'%') ORDER BY o.id DESC")
+	List<Oficina> listar(@Param("param") String param);
 }

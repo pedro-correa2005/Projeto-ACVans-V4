24552df2c5bb70ac4d9ff8aa68c84387c0097881
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 15:08:49 -0300

    Create Auditoria.java
    Modelagem da tabela de auditoria

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Auditoria.java
@@ -0,0 +1,56 @@
+package com.fooengineers.projetoAcVansV4.entity;
+
+import java.sql.Timestamp;
+
+import com.fooengineers.projetoAcVansV4.converter.DetalhesConverter;
+import com.fooengineers.projetoAcVansV4.domain.Detalhes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Auditoria {
+	@Id
+	@Column(name = "id_auditoria")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String acao;
+	
+	@Column
+	private String entidade;
+	
+	@Column(name = "id_registro")
+	private Long idRegistro;
+	
+	@Column(nullable = false)
+	private Timestamp tempo;
+	
+	@Column(name = "enderecoIp")
+	private String enderecoIp;
+	
+	@Convert(converter = DetalhesConverter.class)
+	@Column(columnDefinition = "json")
+	private Detalhes detalhes;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_usuario")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_oficina")
+	private Oficina oficina;
+}

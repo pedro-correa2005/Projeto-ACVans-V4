d0717e32effabfcb9b391033d790efe7a96972e6
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 15:08:40 -0300

    Create DetalhesConverter.java
    Conversor de tipo Detalhes para Json

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/converter/DetalhesConverter.java
@@ -0,0 +1,30 @@
+package com.fooengineers.projetoAcVansV4.converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fooengineers.projetoAcVansV4.domain.Detalhes;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class DetalhesConverter implements AttributeConverter<Detalhes, String>{
+	private final ObjectMapper mapper = new ObjectMapper();
+	
+	@Override
+	public String convertToDatabaseColumn(Detalhes obj) {
+		try {
+			return mapper.writeValueAsString(obj);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Detalhes convertToEntityAttribute(String json) {
+		try {
+			return mapper.readValue(json, Detalhes.class);
+		}catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}

1b745205fdc5134589818a71576a27de1a722c76
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:26:49 -0300

    Update TipoServicoRepository.java
    Queries

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/TipoServicoRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/TipoServicoRepository.java
@@ -1,9 +1,16 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 
 public interface TipoServicoRepository extends JpaRepository<TipoServico, Long> {
-	
+	//Filtragem de tenant
+	List<TipoServico> findByOficina(Oficina oficina);
+	//Pesquisar por id (filtrada por tenant)
+	Optional<TipoServico> findByIdAndOficina(Long id, Oficina oficina);
 }

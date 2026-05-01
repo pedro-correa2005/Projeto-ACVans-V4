6e9223a23e6b9aeefccbb027be02cffec0198e8c
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:29:24 -0300

    Create EtapaServicoRepository.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/EtapaServicoRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
+
+public interface EtapaServicoRepository extends JpaRepository<EtapaServico, Long> {
+
+}

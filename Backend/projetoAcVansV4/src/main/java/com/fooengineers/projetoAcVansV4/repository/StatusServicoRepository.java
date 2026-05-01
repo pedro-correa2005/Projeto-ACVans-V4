f57864289afca6080bb772faf4a7be9ebe33b719
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:47:16 -0300

    Create StatusServicoRepository.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/StatusServicoRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.StatusServico;
+
+public interface StatusServicoRepository extends JpaRepository<StatusServico, Long> {
+
+}

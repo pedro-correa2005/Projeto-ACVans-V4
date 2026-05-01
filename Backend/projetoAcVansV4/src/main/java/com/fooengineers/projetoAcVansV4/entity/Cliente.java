a222ab65dd8bdce05bbd465393884d0e536a56d4
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 15:13:31 -0300

    Update Cliente.java
    Relacionamento com tabela Oficina

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Cliente.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Cliente.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +26,8 @@ public class Cliente {
 	
 	@Column(nullable = false)
 	private String celular;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_oficina", nullable = false)
+	private Oficina oficina;
 }

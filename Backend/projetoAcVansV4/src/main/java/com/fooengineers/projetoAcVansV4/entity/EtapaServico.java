68a877dbc332804f1914eba1f6d7d71367acc218
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 15:30:56 -0300

    Create EtapaServico.java
    Modelagem de entidade etapa de serviço

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/EtapaServico.java
@@ -0,0 +1,39 @@
+package com.fooengineers.projetoAcVansV4.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "etapa_servico")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EtapaServico {
+	@Id
+	@Column(name = "id_etapa_servico")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Integer ordem;
+	
+	@Column(nullable = false)
+	private String titulo;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_tipo_servico", nullable = false)
+	private TipoServico tipoServico;
+	
+	@ManyToOne
+	@JoinColumn(name = "fk_oficina", nullable = false)
+	private Oficina oficina;
+}

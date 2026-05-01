5101872772a4b53f1e63a10ac20e36244e22168f
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:18:50 -0300

    Create ClienteRepository.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<T, ID> {
+
+}

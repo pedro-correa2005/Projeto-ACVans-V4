294578db475a1ff9c7281f3882710dc3ff5df8ac
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:07:50 -0300

    Update UsuarioRepository.java
    Query para busca por email

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/UsuarioRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	//Query para busca por email
+	Optional<Usuario> findByEmail(String email);
 }

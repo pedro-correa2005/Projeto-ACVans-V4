a39935aa7d66b70df3dffeece72f6744a8f6ab83
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:25:20 -0300

    Create TipoServicoRepository.java
    Interface para tipo de serviço

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/TipoServicoRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.TipoServico;
+
+public interface TipoServicoRepository extends JpaRepository<TipoServico, Long> {
+	
+}

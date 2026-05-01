51ce88b679780c2c1b5b25f82657a76c9f48f368
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 17:01:59 -0300

    Update HistoricoEtapaRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
@@ -1,9 +1,19 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.HistoricoEtapa;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.Servico;
 
 public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long> {
-	
+	//Filtrar por servico, etapa e tenant
+	Optional<HistoricoEtapa> findByServicoAndEtapaServicoAndOficina(
+			Servico servico,
+			EtapaServico etapaServico,
+			Oficina oficina
+	);
 }

b22c9fc1255818b8c14ca941e9bef3e1fc024c53
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:57:26 -0300

    Update ServicoRepository.java
    Várias Queries

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ServicoRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ServicoRepository.java
@@ -1,9 +1,22 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.Servico;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
-
+	//Pesquisar por token de atualização
+	Optional<Servico> findByTokenAtualizacao(String tokenAtualizacao);
+	//Pesquisar por token de consulta
+	Optional<Servico> findByTokenConsulta(String tokenConsulta);
+	
+	//Contar serviços por veículo
+	int countByVeiculoPlacaAndOficina(String placa, Oficina oficina);
+	//Filtrar por oficina
+	Page<Servico> findByOficina(Oficina oficina, Pageable pageable);
 }

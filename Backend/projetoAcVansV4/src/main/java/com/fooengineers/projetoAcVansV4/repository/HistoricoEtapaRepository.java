77ae77d9a6dc580529041801c3991aef2049f5b6
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 17:00:38 -0300

    Create HistoricoEtapaRepository.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.HistoricoEtapa;
+
+public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long> {
+	
+}

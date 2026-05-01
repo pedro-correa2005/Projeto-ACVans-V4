7c4f989f502b7309a84a6d5801146f3649f434ec
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:24:37 -0300

    Update VeiculoRepository.java
    Queries de filtragem

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/VeiculoRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/VeiculoRepository.java
@@ -1,9 +1,22 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.fooengineers.projetoAcVansV4.entity.Cliente;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.Veiculo;
 
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+	//Query para filtrar por cliente
+	Page<Veiculo> findByCliente(Cliente c ,Pageable pageable);
 	
+	//Query para filtrar por placa e oficina
+	Optional<Veiculo> findByPlacaAndOficina(String placa, Oficina oficina);
+	
+	//Query para contar número de veículos por cliente
+	long countByCliente(Cliente c);
 }

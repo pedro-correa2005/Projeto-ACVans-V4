8403db0ba1962081208bcb9abb0f49d047473e6d
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:21:39 -0300

    Update AuditoriaRepository.java
    Filtro de Tenant

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/AuditoriaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/AuditoriaRepository.java
@@ -1,9 +1,13 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.Auditoria;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
-
+	//Filtro de Tenant
+	Optional<Auditoria> findByIdAndOficina(Long id, Oficina oficina);
 }

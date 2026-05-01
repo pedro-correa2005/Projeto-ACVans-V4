aa7f1d1a2fc01d8dccd365a0b45f2982b2affe39
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 13:55:45 -0300

    Update Usuario.java
    Anotações lomobk

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Usuario.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Usuario.java
@@ -7,8 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)

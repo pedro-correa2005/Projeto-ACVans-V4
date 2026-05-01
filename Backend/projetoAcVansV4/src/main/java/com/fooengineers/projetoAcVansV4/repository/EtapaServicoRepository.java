f62f5888148f64d5bf8739a293dbea2cc868109a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:32:08 -0300

    Update EtapaServicoRepository.java
    Pesquisa por tipo de servico e ordem (filtrado por tenant)

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/EtapaServicoRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/EtapaServicoRepository.java
@@ -1,9 +1,18 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 
 public interface EtapaServicoRepository extends JpaRepository<EtapaServico, Long> {
-
+	//Pesquisa por tipo de servico e ordem (filtrado por tenant)
+	Optional<EtapaServico> findByTipoServicoAndOrdemAndOficina(
+		TipoServico tipoServico,
+		Integer ordem,
+		Oficina oficina
+	);
 }

f0d5a7e04bfc24cf0315f6b0472c10a73512f5fe
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:49:13 -0300

    Create ServicoRepository.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.fooengineers.projetoAcVansV4.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fooengineers.projetoAcVansV4.entity.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+
+}

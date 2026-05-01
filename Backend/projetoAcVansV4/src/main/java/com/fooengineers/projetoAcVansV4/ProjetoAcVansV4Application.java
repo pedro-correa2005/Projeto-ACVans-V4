e84126e70f476507ac940444310847eb0846aeab
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 00:34:54 -0300

    Inicialização do projeto springboot

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/ProjetoAcVansV4Application.java
@@ -0,0 +1,13 @@
+package com.fooengineers.projetoAcVansV4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcVansV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcVansV4Application.class, args);
+	}
+
+}
---
+++ Backend/projetoAcVansV4/src/test/java/com/fooengineers/projetoAcVansV4/ProjetoAcVansV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooengineers.projetoAcVansV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcVansV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

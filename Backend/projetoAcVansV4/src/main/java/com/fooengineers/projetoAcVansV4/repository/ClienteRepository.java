83996fa877af188736887c219eefd5bf413be5e3
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-01 16:19:26 -0300

    Update ClienteRepository.java
    Filtro de tenant

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
@@ -1,7 +1,13 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<T, ID> {
+import com.fooengineers.projetoAcVansV4.entity.Cliente;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	//Filtro de tenant
+	Optional<Cliente> findByIdClienteAndOficina(Long idCliente, Oficina oficina);
 }

4a021f2771a2e895e86f4479421f53e218358419
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-02 13:23:08 -0300

    Update SecurityConfig.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/SecurityConfig.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/SecurityConfig.java
@@ -1,5 +1,22 @@
 package com.fooengineers.projetoAcVansV4.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
 
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configs) throws Exception{
+		return configs.getAuthenticationManager();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }

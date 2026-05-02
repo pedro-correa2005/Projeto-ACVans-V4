101f40bc2147014000e8477c130b669547b99cb7
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-02 13:20:57 -0300

    Create CustomUserDetailsService.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.fooengineers.projetoAcVansV4.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.fooengineers.projetoAcVansV4.repository.UsuarioRepository;
+
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return usuarioRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário com email " + username + " não encontrado."));
+	}
+
+}

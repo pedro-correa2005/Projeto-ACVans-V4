5e81533810cd51e2b4228a528e6e2491958f47cf
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-02 13:16:02 -0300

    Update Usuario.java
    Implemetanção de UserDetails

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Usuario.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/Usuario.java
@@ -1,6 +1,12 @@
 package com.fooengineers.projetoAcVansV4.entity;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -20,7 +26,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_usuario")
@@ -49,4 +60,21 @@ public class Usuario {
 			inverseJoinColumns = @JoinColumn(name = "fk_role")
 	)
 	private Set<Role> roles;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(role -> new SimpleGrantedAuthority("ROLE_" + role.getNome()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
 }

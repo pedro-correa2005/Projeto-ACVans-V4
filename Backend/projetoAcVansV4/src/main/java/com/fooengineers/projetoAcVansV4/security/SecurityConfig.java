3feda987602ce1fb32bba8717c3423e732182567
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-02 15:58:36 -0300

    Update SecurityConfig.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/SecurityConfig.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/SecurityConfig.java
@@ -1,15 +1,47 @@
 package com.fooengineers.projetoAcVansV4.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.repository.UsuarioRepository;
 
 @Configuration
 public class SecurityConfig {
-
+	
+	@Autowired
+	private JwtAuthenticationFilter jwtFilter;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		return http
+				//Desativa csrf
+				.csrf(csrf -> csrf.disable())
+				//Stateless
+				.sessionManagement(session ->
+					session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				//Regras de acesso
+				.authorizeHttpRequests(auth -> auth
+					//endpoints públicos
+					.requestMatchers("/api/", "/api/login").permitAll()
+					//Qualquer outr endpoint
+					.anyRequest().authenticated()
+				)
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration configs) throws Exception{
 		return configs.getAuthenticationManager();

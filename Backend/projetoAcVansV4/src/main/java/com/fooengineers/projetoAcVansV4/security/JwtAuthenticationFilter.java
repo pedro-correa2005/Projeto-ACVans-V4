95093e7a941a57e5126c4b3ccd0a35403735fc18
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-02 15:35:42 -0300

    Update JwtAuthenticationFilter.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/JwtAuthenticationFilter.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/JwtAuthenticationFilter.java
@@ -2,20 +2,71 @@ package com.fooengineers.projetoAcVansV4.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+	
+	@Autowired
+	private JwtService jwtService; 
+	@Autowired
+	private CustomUserDetailsService userDetailsService; 
 	@Override
+	
+	//Filtro de requisições
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		String token = getTokenFromCookies(request); //Pega token do cookie enviado pela request
+		
+		if(token != null) {
+			try {
+				//Extrai usuário do token
+				String username = jwtService.extractUsername(token);
+				
+				//Se já está autenticado, não refaz
+				if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+					Usuario usuario = (Usuario) userDetailsService.loadUserByUsername(username);
+					if(jwtService.isValid(token, username)) {
+						UsernamePasswordAuthenticationToken auth = 
+								new UsernamePasswordAuthenticationToken(
+										usuario,
+										null,
+										usuario.getAuthorities()
+						);
+						auth.setDetails(
+							new WebAuthenticationDetailsSource().buildDetails(request)
+						);
+						SecurityContextHolder.getContext().setAuthentication(auth);
+					}
+				}
+			} catch (Exception e) {
+				SecurityContextHolder.clearContext();
+			}
+		}
+		filterChain.doFilter(request, response);
 	}
-
+	
+	private String getTokenFromCookies(HttpServletRequest request) {
+		if (request.getCookies() == null) return null;
+		
+		for (Cookie cookie : request.getCookies()) {
+			if("access_token".equals(cookie.getName())) {
+				return cookie.getValue();
+			}
+		}
+		return null;
+	}
 }

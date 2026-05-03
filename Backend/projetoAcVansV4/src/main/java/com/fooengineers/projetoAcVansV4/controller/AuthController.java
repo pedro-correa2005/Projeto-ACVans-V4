6852a5b25fa2c06f3cc2ef4adb0ddb1695b610f7
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-03 02:19:52 -0300

    Update AuthController.java
    Rotação e validação com redis de refresh token

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/AuthController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fooengineers.projetoAcVansV4.dto.LoginRequestDTO;
 import com.fooengineers.projetoAcVansV4.security.JwtService;
+import com.fooengineers.projetoAcVansV4.service.RedisService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -25,6 +26,8 @@ public class AuthController {
 	private AuthenticationManager authenticationManager;
 	@Autowired
 	private JwtService jwtService;
+	@Autowired
+	private RedisService redisService;
 	
 	@Value("${app.security.cookie.secure}")
 	private boolean secure;
@@ -41,6 +44,11 @@ public class AuthController {
 		
 		String accessToken = jwtService.generateAccessToken(email);
 		String refreshToken = jwtService.generateRefreshToken(email);
+		
+		//Salva no redis o identificador único jti:email com tempo de expiração igual ao token
+		String jti = jwtService.extractJti(refreshToken);
+		long ttl = 60 * 60 * 24;
+		redisService.saveRefreshToken(jti, email, System.currentTimeMillis(), ttl);
 		
 		ResponseCookie accessCookie = ResponseCookie.from("access_token", accessToken)
 				.httpOnly(true)
@@ -66,30 +74,66 @@ public class AuthController {
 	
 	@PostMapping("/refresh")
 	public ResponseEntity<?> refreshToken(HttpServletRequest request){
+		//Pega o token no cookie da requisição
 		String refreshToken = jwtService.getTokenFromCookies(request, "refresh_token");
 		
+		//Verifica se o token está no cookie
 		if(refreshToken == null) {
 			return ResponseEntity.status(401).body("Refresh token ausente");
 		}
 		
-		String username = jwtService.extractUsername(refreshToken, "refresh_token");
-		
-		if(!jwtService.isValid(refreshToken, username, "refresh_token")) {
+		//Verifica se a chave do token é válida e se o token já não expirou
+		if(!jwtService.isValid(refreshToken, "refresh_token")) {
 			return ResponseEntity.status(401).body("Refresh token inválido");
 		}
 		
-		String newAccessToken = jwtService.generateAccessToken(username);
+		//Validação no redis, caso ambas validações tenham sido burladas
+		String jti = jwtService.extractJti(refreshToken);
+		if(!redisService.exists(jti)) {
+			return ResponseEntity.status(401).body("Refresh token revogado");
+		}
 		
-		ResponseCookie cookie = ResponseCookie.from("access_token", newAccessToken)
+		//Verifica tempo máximo de sessão(milissegundos)
+		long maxSessionTime = 1000 * 60 * 60 * 24;
+		long createdAt = redisService.getCreatedAt(jti);
+		if(System.currentTimeMillis() - createdAt > maxSessionTime) {
+			redisService.delete(jti);
+			return ResponseEntity.status(401).body("Sessão expirada");
+		}
+		
+		//Rotação: invalida token antigo
+		redisService.delete(jti);
+		
+		//Gera novos tokens
+		String username = jwtService.extractUsername(refreshToken, "refresh_token");
+		String newAccessToken = jwtService.generateAccessToken(username);
+		String newRefreshToken = jwtService.generateRefreshToken(username);
+		
+		String newJti = jwtService.extractJti(newRefreshToken);
+		
+		//Salva novo refresh no Redis
+		long ttl = 60 * 60 * 24;
+		redisService.saveRefreshToken(newJti, username, createdAt, ttl);
+		
+		ResponseCookie accessCookie = ResponseCookie.from("access_token", newAccessToken)
 				.httpOnly(true)
 				.secure(secure)
 				.sameSite("None")
 				.path("/")
 				.maxAge(60 * 10)
 				.build();
+
+		ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", newAccessToken)
+				.httpOnly(true)
+				.secure(secure)
+				.sameSite("None")
+				.path("/")
+				.maxAge(60 * 60 * 24)
+				.build();
 		
 		return ResponseEntity.ok()
-				.header(HttpHeaders.SET_COOKIE, cookie.toString())
+				.header(HttpHeaders.SET_COOKIE, accessCookie.toString())
+				.header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
 				.body("Token Renovado");
 	}
 }

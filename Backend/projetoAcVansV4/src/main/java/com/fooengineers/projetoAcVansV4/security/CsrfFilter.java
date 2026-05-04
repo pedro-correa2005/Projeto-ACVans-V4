7789a28cf1703cf59f644d16e8d8c0089d914af8
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-04 00:52:29 -0300

    Create CsrfFilter.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/CsrfFilter.java
@@ -0,0 +1,40 @@
+package com.fooengineers.projetoAcVansV4.security;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CsrfFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		String csrfHeader = request.getHeader("X-CSRF-TOKEN");
+		String csrfCookie = getCookieValue(request, "csrf_token");
+		if (csrfHeader == null || csrfCookie == null || !csrfHeader.equals(csrfCookie)) {
+		    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		    response.getWriter().write("CSRF inválido");
+		    return;
+		}
+		filterChain.doFilter(request, response);
+	}
+	
+	private String getCookieValue(HttpServletRequest request, String name) {
+		if (request.getCookies() == null) return null;
+		
+		for (Cookie cookie : request.getCookies()) {
+		    if (cookie.getName().equals(name)) {
+		        return cookie.getValue();
+		    }
+		}
+		return null;
+	}
+
+}

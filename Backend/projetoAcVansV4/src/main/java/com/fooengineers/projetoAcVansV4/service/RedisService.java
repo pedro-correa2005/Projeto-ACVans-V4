628cf0c7d1892092417d06b074982b534958e196
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-04 18:49:45 -0300

    Update RedisService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/RedisService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/RedisService.java
@@ -3,6 +3,7 @@ package com.fooengineers.projetoAcVansV4.service;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -10,29 +11,51 @@ import org.springframework.stereotype.Service;
 public class RedisService {
 	
 	@Autowired
-	private StringRedisTemplate redisTemplate;
+	private StringRedisTemplate stringRedisTemplate;
+	
+	@Autowired
+	private RedisTemplate<String, Integer> integerRedisTemplate;
+
 	
 	public void saveRefreshToken(String jti, String username, long createdAt, long ttlSeconds) {
 		String value = username + "|" + createdAt;
-		redisTemplate.opsForValue().set(jti, value, ttlSeconds, TimeUnit.SECONDS);
+		stringRedisTemplate.opsForValue().set(jti, value, ttlSeconds, TimeUnit.SECONDS);
+	}
+	
+	public void save2FACode(String email, String code, long ttlSeconds) {
+		stringRedisTemplate.opsForValue().set(email, code, ttlSeconds, TimeUnit.SECONDS);
+	}
+	
+	public void save2FASession(String tempToken, String email, long ttlSeconds) {
+		stringRedisTemplate.opsForValue().set(tempToken, email, ttlSeconds, TimeUnit.SECONDS);
+	}
+	
+	public void save2FAAttempts(String tempToken, long ttlSeconds) {
+		String key = "2fa_attempts:" + tempToken;
+		integerRedisTemplate.opsForValue().set(key, 0, ttlSeconds, TimeUnit.SECONDS);
+	}
+	
+	public long increment2FAAttempts(String tempToken) {
+		String key = "2fa_attempts:" + tempToken;
+		return integerRedisTemplate.opsForValue().increment(key);
 	}
 	
 	public long getCreatedAt(String jti) {
-		String value = redisTemplate.opsForValue().get(jti);
+		String value = stringRedisTemplate.opsForValue().get(jti);
 		if (value == null) return 0;
 		String[] parts = value.split("\\|");
 		return Long.parseLong(parts[1]);
 	}
 	
 	public String get(String key) {
-		return redisTemplate.opsForValue().get(key);
+		return stringRedisTemplate.opsForValue().get(key);
 	}
 	
 	public boolean exists(String key) {
-		return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+		return Boolean.TRUE.equals(stringRedisTemplate.hasKey(key));
 	}
 	
 	public void delete(String key) {
-		redisTemplate.delete(key);
+		stringRedisTemplate.delete(key);
 	}
 }

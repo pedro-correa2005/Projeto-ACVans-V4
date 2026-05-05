9afee2dc077ef58e26f00319591e6f538b7c9a9f
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-04 23:10:06 -0300

    Update EmailService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/EmailService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/EmailService.java
@@ -1,5 +1,23 @@
 package com.fooengineers.projetoAcVansV4.service;
 
-public class EmailService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+public class EmailService {
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void enviar2FACode(String email, String code) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		message.setTo(email);
+		message.setSubject("Seu código de verificação");
+		message.setText("Seu código 2FA é: " + code + "/n"
+				+ "Expira em 5 minutos.");
+		
+		mailSender.send(message);
+	}
 }

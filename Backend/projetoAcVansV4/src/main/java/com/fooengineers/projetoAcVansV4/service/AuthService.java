4dffe4e80d4360d961564c12f77ee61386876342
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-05 11:55:42 -0300

    Update AuthService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuthService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuthService.java
@@ -1,5 +1,29 @@
 package com.fooengineers.projetoAcVansV4.service;
 
-public class AuthService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
 
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.security.CustomUserDetailsService;
+
+
+@Service
+public class AuthService {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+	
+	public Usuario validarCredenciais(String email, String senha) {
+		//Verifica credenciais e pega usuário
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, senha));
+		}catch (AuthenticationException e) {
+			return null;
+		}
+		return (Usuario) userDetailsService.loadUserByUsername(email);
+	}
 }

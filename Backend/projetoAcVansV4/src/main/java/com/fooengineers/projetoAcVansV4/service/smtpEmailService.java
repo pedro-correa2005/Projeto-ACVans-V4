d24c039231071595428365d2cea18a08c49c67e1
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-04 23:59:59 -0300

    Update smtpEmailService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/smtpEmailService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/smtpEmailService.java
@@ -1,12 +1,16 @@
 package com.fooengineers.projetoAcVansV4.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmailService {
+public class smtpEmailService {
 	@Autowired
 	private JavaMailSender mailSender;
 	
@@ -15,9 +19,18 @@ public class EmailService {
 		
 		message.setTo(email);
 		message.setSubject("Seu código de verificação");
-		message.setText("Seu código 2FA é: " + code + "/n"
+		message.setText("Seu código 2FA é: " + code + "\n"
 				+ "Expira em 5 minutos.");
 		
-		mailSender.send(message);
+		mailSender.send();
+	}
+	
+	public String load2FATemplate(String code) throws IOException{
+		InputStream is = getClass()
+				.getClassLoader()
+				.getResourceAsStream("templates/2fa-template.html");
+		String html = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+		
+		return html.replace("{{CODE}}", code);
 	}
 }

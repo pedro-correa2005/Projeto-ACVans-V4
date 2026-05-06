932495b0bf74edaf82da02be364e43e99cb5edab
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-06 12:12:48 -0300

    Create EmailDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/EmailDTO.java
@@ -0,0 +1,19 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailDTO {
+	@NotBlank(message = "Email é orbigatório")
+	@Pattern(
+	        regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", 
+	        message = "E-mail inválido"
+	    )
+	private String email;
+}

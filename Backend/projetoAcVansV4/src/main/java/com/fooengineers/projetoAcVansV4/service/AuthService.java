a1d1e27d37cc0350a5ffd4b6ff9d18d5b3cc7f8d
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-06 12:13:23 -0300

    Update AuthService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuthService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuthService.java
@@ -1,13 +1,19 @@
 package com.fooengineers.projetoAcVansV4.service;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
 import com.fooengineers.projetoAcVansV4.security.CustomUserDetailsService;
+
+import jakarta.mail.MessagingException;
 
 
 @Service
@@ -18,6 +24,8 @@ public class AuthService {
 	private CustomUserDetailsService userDetailsService;
 	@Autowired
 	private RedisService redisService;
+	@Autowired
+	private smtpEmailService emailService;
 	
 	public Usuario validarCredenciais(String email, String senha) {
 		//Verifica credenciais e pega usuário
@@ -45,5 +53,32 @@ public class AuthService {
 			return false;
 		}
 		return true;
+	}
+	
+	public void esqueciSenha(String email) {
+		//Valida Email
+		try{
+			userDetailsService.loadUserByUsername(email);
+		} catch (UsernameNotFoundException e) {
+			System.err.println(e);
+			return;
+		}
+		//Gera token e salva no banco relacionado a email
+		String token = UUID.randomUUID().toString();
+		
+		redisService.save("reset:" + token, email, 900);
+		//Envia o email de recuperação
+		try {
+			emailService.enviarResetSenha(email, token);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Usuario validarResetToken(String token) {
+		String email = redisService.get("reset:"+token);
+		return (Usuario) userDetailsService.loadUserByUsername(email);
 	}
 }

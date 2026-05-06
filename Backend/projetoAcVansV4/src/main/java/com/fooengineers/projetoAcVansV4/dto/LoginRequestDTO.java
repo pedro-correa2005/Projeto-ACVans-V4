af18d0003f72d72241e75d831b0be79a9a10732c
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-06 12:12:59 -0300

    Update LoginRequestDTO.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/LoginRequestDTO.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/LoginRequestDTO.java
@@ -1,6 +1,7 @@
 package com.fooengineers.projetoAcVansV4.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequestDTO {
 	@NotBlank(message = "Email é obrigatório")
+	@Pattern(
+	        regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", 
+	        message = "E-mail inválido"
+	    )
 	private String email;
 	@NotBlank(message = "Senha é obrigatória")
 	private String senha;

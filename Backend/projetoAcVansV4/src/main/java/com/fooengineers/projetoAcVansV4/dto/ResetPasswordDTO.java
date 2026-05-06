9563eac84bec31569cf52e5fd102809208502657
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-06 12:13:13 -0300

    Create ResetPasswordDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/ResetPasswordDTO.java
@@ -0,0 +1,18 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResetPasswordDTO {
+	@NotBlank(message = "Token é obrigatório")
+	private String token;
+	@NotBlank(message = "Nova senha é obrigatória")
+	private String novaSenha;
+	@NotBlank(message = "Repetir nova senha é obrigatória")
+	private String repetirNovaSenha;
+}

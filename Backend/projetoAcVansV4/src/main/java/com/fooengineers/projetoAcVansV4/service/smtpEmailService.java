a891c6435201649258c6d657835b5e116427343b
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-06 12:13:33 -0300

    Update smtpEmailService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/smtpEmailService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/smtpEmailService.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,10 @@ import jakarta.mail.internet.MimeMessage;
 public class smtpEmailService {
 	@Autowired
 	private JavaMailSender mailSender;
+	@Autowired
+	@Value("${app.base-url}")
+	private String urlSite;
+	
 	
 	public void enviar2FACode(String email, String code) throws MessagingException, IOException {
 		MimeMessage message = mailSender.createMimeMessage();
@@ -35,5 +40,27 @@ public class smtpEmailService {
 		String html = new String(is.readAllBytes(), StandardCharsets.UTF_8);
 		
 		return html.replace("{{CODE}}", code);
+	}
+	
+	public void enviarResetSenha(String email, String token) throws MessagingException, IOException  {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		helper.setTo(email);
+		helper.setSubject("Redefinição de senha");
+		
+		String link = urlSite + "/auth/reset-senha?token=" + token;
+		
+		helper.setText(loadResetSenhaTemplate(link), true);
+		
+		mailSender.send(message);
+	}
+	
+	public String loadResetSenhaTemplate(String link) throws IOException{
+		InputStream is = getClass()
+				.getClassLoader()
+				.getResourceAsStream("templates/reset-senha-template.html");
+		String html = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+		
+		return html.replace("{{LINK}}", link);
 	}
 }

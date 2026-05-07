15057104accb2250660667e6358b40b6d675ad47
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-07 00:40:51 -0300

    Create PrimeiroLoginFilter.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/security/PrimeiroLoginFilter.java
@@ -0,0 +1,53 @@
+package com.fooengineers.projetoAcVansV4.security;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.service.UsuarioService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class PrimeiroLoginFilter extends OncePerRequestFilter{
+	
+	@Autowired
+	private JwtService jwtService;
+	@Autowired
+	private UsuarioService usuarioService;
+
+	private static final List<String> ALLOWED_PATHS = List.of(
+		"/api/auth/mudar-senha",
+		"/api/auth/logout"
+	);
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+		String path = request.getRequestURI();
+		
+		if(ALLOWED_PATHS.contains(path)) {
+			filterChain.doFilter(request, response);
+		}
+		
+		String token = jwtService.getTokenFromCookies(request, "access_token");
+		
+		if(token != null) {
+			String email = jwtService.extrairUsername(token, "access_token");
+			Usuario usuario = usuarioService.buscarPorEmail(email);
+			if(usuario.isPrimeiroLogin()) {
+				response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+				response.getWriter().write("É necessário alterar a senha");
+				return;
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}

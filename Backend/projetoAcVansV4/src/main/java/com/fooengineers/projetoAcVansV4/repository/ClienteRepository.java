ef453f6907605f6271acc043d216b0efab2c8a7a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 00:51:52 -0300

    Update ClienteRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
@@ -1,13 +1,12 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
-import java.util.Optional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.Cliente;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-	//Filtro de tenant
-	Optional<Cliente> findByIdAndOficina(Long id, Oficina oficina);
+	Page<Cliente> findByOficina(Oficina oficina, Pageable pageable);
 }

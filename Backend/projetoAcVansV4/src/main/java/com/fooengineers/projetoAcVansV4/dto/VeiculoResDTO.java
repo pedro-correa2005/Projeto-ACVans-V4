476b368603e3a6c9dbe23e3965798ee8b9a3e712
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 14:37:45 -0300

    Create VeiculoResDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/VeiculoResDTO.java
@@ -0,0 +1,25 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import com.fooengineers.projetoAcVansV4.entity.Veiculo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VeiculoResDTO {
+	private Long id;
+	private String placa;
+	private String marca;
+	private String modelo;
+	
+	//Constructor a partir de entity.Veiculo
+	public VeiculoResDTO(Veiculo veiculo) {
+		this.id = veiculo.getId();
+		this.placa = veiculo.getPlaca();
+		this.marca = veiculo.getMarca();
+		this.modelo = veiculo.getModelo();
+	}
+}

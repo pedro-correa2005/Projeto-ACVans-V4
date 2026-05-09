1da951739d9d91908570b5f81de61a2d760bb731
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 01:21:13 -0300

    Create OficinaResDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/OficinaResDTO.java
@@ -0,0 +1,22 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OficinaResDTO {
+	private int id;
+	private String nome;
+	private boolean ativo;
+	
+	public OficinaResDTO(Oficina oficina) {
+		this.id = oficina.getId();
+		this.nome = oficina.getNome();
+		this.ativo = oficina.getAtivo();
+	}
+}

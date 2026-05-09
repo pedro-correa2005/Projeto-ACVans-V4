d067fc2b76ffff290a00f366b52ef50f08674aaa
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 14:18:54 -0300

    Update CadastroReqDTO.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/CadastroReqDTO.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/CadastroReqDTO.java
@@ -4,6 +4,8 @@ import java.sql.Timestamp;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,18 +14,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CadastroReqDTO {
-	@NotNull(message = "Nome do cliente é obrigatório")
+	@NotBlank(message = "Nome do cliente é obrigatório")
 	private String nomeCliente;
-	@NotNull(message = "Celular do cliente é obrigatório")
+	
+	@NotBlank(message = "O número de celular é obrigatório")
+	@Size(min = 14, max = 14, message = "O número de celular deve ter 14 caracteres")
+	@Pattern(regexp = "^\\([1-9][1-9]\\)\\ 9[0-9]{8}$", message = "Número inválido.")
 	private String celularCliente;
-	@NotNull(message = "Placa do veículo é obrigatório")
+	
+	@NotBlank(message = "A placa é obriatória")
+	@Size(min = 7, max = 7, message = "A placa deve ter 7 caracteres")
+	@Pattern(regexp = "^[A-Z]{3}[0-9]{1}[A-Z0-9]{1}[0-9]{2}$", message = "Placa Inválida")
 	private String placaVeiculo;
-	@NotNull(message = "Marca do veículo é obrigatório")
+	
+	@NotBlank(message = "Marca do veículo é obrigatório")
 	private String marcaVeiculo;
-	@NotNull(message = "Modelo do veículo é obrigatório")
+	
+	@NotBlank(message = "Modelo do veículo é obrigatório")
 	private String modeloVeiculo;
-	@NotBlank(message = "Receber Notificação: Selecione uma opção")
+	
+	@NotNull(message = "Receber Notificação: Selecione uma opção")
 	private boolean receberNotificacao;
+	
 	@NotNull(message = "Data de fim prevista é obrigatório")
 	private Timestamp dataFim;
 }

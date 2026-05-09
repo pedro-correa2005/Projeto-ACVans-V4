016350a488c379cbdfafda049180be11204b2554
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 00:51:36 -0300

    Update AuditoriaRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/AuditoriaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/AuditoriaRepository.java
@@ -1,13 +1,12 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
-import java.util.Optional;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.Auditoria;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {
-	//Filtro de Tenant
-	Optional<Auditoria> findByIdAndOficina(Long id, Oficina oficina);
+	Page<Auditoria> findByOficina(Oficina oficina, Pageable pageable);
 }

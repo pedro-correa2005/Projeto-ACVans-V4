30a9f9f4f781e38238d737051f29b7cc52ca7b04
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 14:21:56 -0300

    Create TipoServicoReqDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/TipoServicoReqDTO.java
@@ -0,0 +1,14 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TipoServicoReqDTO {
+	@NotBlank(message = "Descricao é obrigatória")
+	String descricao;
+}

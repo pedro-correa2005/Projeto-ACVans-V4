c0274e07cfc4ced55b5b3b56373c4a19f976c33a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-09 14:50:06 -0300

    Create AuditoriaDTO.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/AuditoriaDTO.java
@@ -0,0 +1,32 @@
+package com.fooengineers.projetoAcVansV4.dto;
+
+import com.fooengineers.projetoAcVansV4.entity.Auditoria;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuditoriaDTO {
+	private String acao;
+	private String entidade;
+	private Long idRegistro;
+	private String tempo;
+	private String enderecoIp;
+	private String detalhes;
+	private String email;
+	
+	//Constructor a partir de entity.Auditoria
+	public AuditoriaDTO(Auditoria auditoria) {
+		this.acao = auditoria.getAcao();
+		this.entidade = auditoria.getEntidade();
+		this.idRegistro = auditoria.getIdRegistro();
+		this.tempo = auditoria.getTempo().toString();
+		this.enderecoIp = auditoria.getEnderecoIp();
+		this.detalhes = auditoria.getDetalhes().getAntes().toString()
+					+ auditoria.getDetalhes().getDepois().toString();
+		this.email = auditoria.getUsuario().getEmail();
+	}
+}

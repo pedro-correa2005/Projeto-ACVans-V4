95ed0690f1a89bd41aa2fbf30673d4cd54a2fb89
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-10 15:45:47 -0300

    Update AdminController.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/AdminController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,5 +41,11 @@ public class AdminController {
 	public ResponseEntity<OficinaResDTO> atualizar(@ModelAttribute @Valid OficinaReqDTO dto, @PathVariable(required=true) Long idOficina){
 		OficinaResDTO atualizado = oficinaService.atualizar(dto, idOficina);
 		return ResponseEntity.ok(atualizado);
+	}
+	
+	@DeleteMapping("/oficinas/{idOficina}")
+	public ResponseEntity<?> deletar(@PathVariable(required=true) Long idOficina){
+		oficinaService.deletar(idOficina);
+		return ResponseEntity.ok().build();
 	}
 }

5fe45bd1e3e0502440c609b3abd7101ce43dc24d
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-10 16:01:37 -0300

    Update UsuarioService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/UsuarioService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/UsuarioService.java
@@ -1,11 +1,16 @@
 package com.fooengineers.projetoAcVansV4.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.fooengineers.projetoAcVansV4.dto.UsuarioResDTO;
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
 import com.fooengineers.projetoAcVansV4.repository.UsuarioRepository;
+import com.fooengineers.projetoAcVansV4.specification.UsuarioSpecification;
 
 @Service
 public class UsuarioService {
@@ -13,6 +18,12 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepository;
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	
+	public List<UsuarioResDTO> listarPorOficina(Long idOficina, String param){
+		return usuarioRepository.findAll(UsuarioSpecification.filtroGeral(idOficina, param)).stream()
+				.map(UsuarioResDTO::new)
+				.collect(Collectors.toList());
+	}
 	
 	public Usuario buscarPorEmail(String email) {
 		return usuarioRepository.findByEmail(email).orElseThrow();

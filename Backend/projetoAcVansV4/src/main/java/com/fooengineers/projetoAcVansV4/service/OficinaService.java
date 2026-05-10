5aa3c071923f792db87c458a4839f16754ec1835
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-10 15:03:05 -0300

    Update OficinaService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/OficinaService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/OficinaService.java
@@ -8,14 +8,16 @@ import org.springframework.stereotype.Service;
 
 import com.fooengineers.projetoAcVansV4.dto.OficinaResDTO;
 import com.fooengineers.projetoAcVansV4.repository.OficinaRepository;
+import com.fooengineers.projetoAcVansV4.specification.OficinaSpecification;
 
 @Service
 public class OficinaService {
 	@Autowired
 	OficinaRepository oficinaRepository;
+	
 	public List<OficinaResDTO> listar(String param){
-		return oficinaRepository.findAll()
-				.stream().map(OficinaResDTO::new)
+		return oficinaRepository.findAll(OficinaSpecification.filtroGeral(param)).stream()
+				.map(OficinaResDTO::new)
 				.collect(Collectors.toList());
 		
 	}

5646c89996b804d4a20fd3a93529a79af68fec34
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-11 20:54:01 -0300

    Update TipoServicoService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/TipoServicoService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/TipoServicoService.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fooengineers.projetoAcVansV4.dto.TipoServicoReqDTO;
 import com.fooengineers.projetoAcVansV4.dto.TipoServicoResDTO;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 import com.fooengineers.projetoAcVansV4.repository.TipoServicoRepository;
 
 @Service
@@ -19,5 +21,13 @@ public class TipoServicoService {
 		return tipoServicoRepository.findByOficina(oficina).stream()
 				.map(TipoServicoResDTO::new)
 				.collect(Collectors.toList());
+	}
+	
+	public TipoServicoResDTO criar(TipoServicoReqDTO dto, Oficina oficina) {
+		TipoServico tipo = new TipoServico();
+		tipo.setDescricao(dto.getDescricao());
+		tipo.setOficina(oficina);
+		TipoServico criado = tipoServicoRepository.save(tipo);
+		return new TipoServicoResDTO(criado);
 	}
 }

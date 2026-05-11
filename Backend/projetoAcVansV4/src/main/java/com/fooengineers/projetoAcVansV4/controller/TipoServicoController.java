405af484a41f6e7ce0776361e67f403e45f48d12
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-11 20:54:21 -0300

    Update TipoServicoController.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/TipoServicoController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/TipoServicoController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fooengineers.projetoAcVansV4.dto.TipoServicoReqDTO;
 import com.fooengineers.projetoAcVansV4.dto.TipoServicoResDTO;
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
 import com.fooengineers.projetoAcVansV4.service.TipoServicoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tipos-servico")
@@ -22,5 +27,11 @@ public class TipoServicoController {
 	public List<TipoServicoResDTO> listar(Authentication authentication){
 		Usuario usuario = (Usuario) authentication.getPrincipal();
 		return tipoServicoService.listar(usuario.getOficina());
+	}
+	
+	@PostMapping
+	public TipoServicoResDTO criar(@ModelAttribute @Valid TipoServicoReqDTO dto, Authentication authentication) {
+		Usuario usuario = (Usuario) authentication.getPrincipal();
+		return tipoServicoService.criar(dto, usuario.getOficina());
 	}
 }

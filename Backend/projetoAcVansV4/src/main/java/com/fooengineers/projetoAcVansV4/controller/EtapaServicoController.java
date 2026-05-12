cf2005439017365ac872d503267ad415338511fe
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-11 21:53:27 -0300

    Update EtapaServicoController.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/EtapaServicoController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/EtapaServicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,18 +18,23 @@ import com.fooengineers.projetoAcVansV4.service.EtapaServicoService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/tipos-servico")
+@RequestMapping("/api")
 public class EtapaServicoController {
 	@Autowired
 	EtapaServicoService etapaServicoService;
 	
-	@GetMapping("/{idTipoServico}/etapas-servico")
+	@GetMapping("/tipos-servico/{idTipoServico}/etapas-servico")
 	public List<EtapaServicoResDTO> listar(@PathVariable(required=true) Long idTipoServico){
 		return etapaServicoService.listarPorTipo(idTipoServico);
 	}
 	
-	@PostMapping("/{idTipoServico}/etapas-servico")
+	@PostMapping("/tipos-servico/{idTipoServico}/etapas-servico")
 	public EtapaServicoResDTO criar(@ModelAttribute @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idTipoServico) {
 		return etapaServicoService.criar(dto, idTipoServico);
+	}
+	
+	@PutMapping("/api/etapas-servico/{idEtapaServico}")
+	public EtapaServicoResDTO atualizar(@ModelAttribute @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idEtapaServico) {
+		return etapaServicoService.atualizar(dto, idEtapaServico);
 	}
 }

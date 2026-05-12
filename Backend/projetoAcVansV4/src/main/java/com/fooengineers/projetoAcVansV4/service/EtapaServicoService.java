204d93028520b9c78846ae577a2ff42ed712879f
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-11 21:39:48 -0300

    Update EtapaServicoService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/EtapaServicoService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/EtapaServicoService.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fooengineers.projetoAcVansV4.dto.EtapaServicoReqDTO;
 import com.fooengineers.projetoAcVansV4.dto.EtapaServicoResDTO;
+import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 import com.fooengineers.projetoAcVansV4.exception.TipoServicoNaoEncontradoException;
 import com.fooengineers.projetoAcVansV4.repository.EtapaServicoRepository;
@@ -29,5 +31,24 @@ public class EtapaServicoService {
 		return etapaServicoRepository.findByTipoServico(tipo)
 				.stream().map(EtapaServicoResDTO::new)
 				.collect(Collectors.toList());
+	}
+	
+	public EtapaServicoResDTO criar(EtapaServicoReqDTO dto, Long idTipoServico) {
+		TipoServico tipo = tipoServicoRepository.findById(idTipoServico).orElseThrow(() -> new TipoServicoNaoEncontradoException(idTipoServico));
+		EtapaServico etapa = new EtapaServico();
+		EtapaServico ultimaEtapa = etapaServicoRepository.findFirstByTipoServicoOrderByOrdemDesc(tipo).orElse(null);
+		int ordem = 100;
+		if(ultimaEtapa != null) {
+			ordem = ultimaEtapa.getOrdem() + 100;
+		}
+		etapa.setTitulo(dto.getTitulo());
+		etapa.setDescricao(dto.getDescricao());
+		etapa.setOrdem(ordem);
+		etapa.setTipoServico(tipo);
+		etapa.setOficina(tipo.getOficina());
+		
+		EtapaServico criado = etapaServicoRepository.save(etapa);
+		
+		return new EtapaServicoResDTO(criado);
 	}
 }

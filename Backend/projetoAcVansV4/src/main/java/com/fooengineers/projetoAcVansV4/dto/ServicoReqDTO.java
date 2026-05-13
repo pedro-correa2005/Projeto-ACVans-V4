9116059ac889af7bd1ffc5b289ad5e7e3de8192d
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 20:20:16 -0300

    Update ServicoReqDTO.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/ServicoReqDTO.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/ServicoReqDTO.java
@@ -2,7 +2,6 @@ package com.fooengineers.projetoAcVansV4.dto;
 
 import java.sql.Timestamp;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,12 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ServicoReqDTO {
-	@NotBlank(message = "Tipo de serviço é obrigatório")
+	@NotNull(message = "Tipo de serviço é obrigatório")
 	private Long idTipoServico;
 	@NotNull(message = "Receber Notificação: Selecione uma opção")
 	private boolean receberNotificacao;
-	@NotBlank(message = "Tipo de serviço é obrigatório")
-	private Long idStatusServico;
+	@NotNull(message = "Tipo de serviço é obrigatório")
+	private Integer idStatusServico;
 	@NotNull(message = "Data de fim prevista é obrigatória")
 	private Timestamp dataFim;
+	@NotNull(message = "Veículo é obrigatório")
+	private Long idVeiculo;
 }

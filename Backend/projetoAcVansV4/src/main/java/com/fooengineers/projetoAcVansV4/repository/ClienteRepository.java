4751c2c0412c585341c064c015a5b9eef74bd6e8
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 13:19:16 -0300

    Update ClienteRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/ClienteRepository.java
@@ -3,10 +3,12 @@ package com.fooengineers.projetoAcVansV4.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.fooengineers.projetoAcVansV4.entity.Cliente;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long>, JpaSpecificationExecutor<Cliente>{
 	Page<Cliente> findByOficina(Oficina oficina, Pageable pageable);
+	Page<Cliente> findByOficinaAndNomeContainingIgnoreCase(Oficina oficina, String nome, Pageable pageable);
 }

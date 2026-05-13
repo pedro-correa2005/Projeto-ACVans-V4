cd1c4ea181b2fb80dc12074d93eea94358a497d2
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 15:08:00 -0300

    Update ServicoSpecification.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/specification/ServicoSpecification.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/specification/ServicoSpecification.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
 
+import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.Servico;
+import com.fooengineers.projetoAcVansV4.entity.StatusServico;
+import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 import com.fooengineers.projetoAcVansV4.entity.Veiculo;
 
 import jakarta.persistence.criteria.Join;
@@ -49,6 +52,26 @@ public class ServicoSpecification {
 				);
 			}
 			return cb.and(predicates.toArray(new Predicate[0]));
+		};
+	}
+	public static Specification<Servico> filtrarPorVeiculo(String termo, Long idVeiculo){
+		return(root, query, cb) -> {
+			query.distinct(true);
+			Join<Servico, TipoServico> tipoServico = root.join("tipoServico");
+			Join<Servico, EtapaServico> etapaServico = root.join("etapaServico");
+			Join<Servico, StatusServico> statusServico = root.join("statusServico");
+			
+			if(termo == null || termo.isBlank()) {
+				return cb.equal(root.get("veiculo").get("id"), idVeiculo);
+			}
+			String like = "%" + termo.toLowerCase() + "%";
+			
+			Predicate porTipo = cb.like(cb.lower(tipoServico.get("descricao")), like);
+			Predicate porStatus = cb.like(cb.lower(statusServico.get("descricao")), like);
+			Predicate porEtapa = cb.like(cb.lower(etapaServico.get("descricao")), like);
+			
+			return cb.and(cb.equal(root.get("veiculo").get("id"), idVeiculo),
+					cb.or(porTipo, porStatus, porEtapa));
 		};
 	}
 }

c2214091e42273b1b0cb3115fecdb32244eb70d8
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 20:20:31 -0300

    Create ServicoController.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/ServicoController.java
@@ -0,0 +1,38 @@
+package com.fooengineers.projetoAcVansV4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fooengineers.projetoAcVansV4.dto.ServicoReqDTO;
+import com.fooengineers.projetoAcVansV4.dto.ServicoResDTO;
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.service.ServicoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/servicos")
+public class ServicoController {
+	@Autowired
+	private ServicoService servicoService;
+	@GetMapping
+	public Page<ServicoResDTO> listar(Authentication authentication, Pageable pageable){
+		Usuario usuario = (Usuario) authentication.getPrincipal();
+		return servicoService.listar(usuario.getOficina(), pageable);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ServicoResDTO> criar(@ModelAttribute @Valid ServicoReqDTO dto, Authentication authentication){
+		Usuario usuario = (Usuario) authentication.getPrincipal();
+		ServicoResDTO criado = servicoService.criar(dto, usuario.getOficina().getId());
+		return ResponseEntity.ok(criado);
+	}
+}

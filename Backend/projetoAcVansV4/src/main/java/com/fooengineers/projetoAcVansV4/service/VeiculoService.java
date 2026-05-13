93e29cf2078e2ea93e3d975d38beb8df70b2622a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 13:33:12 -0300

    Create VeiculoService.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/VeiculoService.java
@@ -0,0 +1,35 @@
+package com.fooengineers.projetoAcVansV4.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.fooengineers.projetoAcVansV4.dto.VeiculoResDTO;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.Veiculo;
+import com.fooengineers.projetoAcVansV4.exception.ClienteNaoEncontradoExcepiton;
+import com.fooengineers.projetoAcVansV4.repository.ClienteRepository;
+import com.fooengineers.projetoAcVansV4.repository.VeiculoRepository;
+import com.fooengineers.projetoAcVansV4.specification.VeiculoSpecification;
+
+@Service
+public class VeiculoService {
+	@Autowired
+	VeiculoRepository veiculoRepository;
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	public Page<VeiculoResDTO> listar(Oficina oficina, Pageable pageable) {
+		return null;
+	}
+
+	public List<VeiculoResDTO> buscarPorCliente(Long idCliente, String termo) {
+		clienteRepository.findById(idCliente).orElseThrow(() -> new ClienteNaoEncontradoExcepiton(idCliente));
+		List<Veiculo> veiculos = veiculoRepository.findAll(VeiculoSpecification.filtrarPorCliente(termo, idCliente));
+		return veiculos.stream().map(VeiculoResDTO::new).collect(Collectors.toList());
+	}
+}

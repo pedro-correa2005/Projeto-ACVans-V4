2324131431788b88010c402c8a41e88cf11d533b
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 14:02:48 -0300

    Update VeiculoController.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/VeiculoController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/VeiculoController.java
@@ -3,10 +3,13 @@ package com.fooengineers.projetoAcVansV4.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +34,15 @@ public class VeiculoController {
 	}
 	
 	@PostMapping
-	public VeiculoResDTO criar(Authentication authentication, @ModelAttribute @Valid VeiculoReqDTO dto) {
+	public ResponseEntity<VeiculoResDTO> criar(Authentication authentication, @ModelAttribute @Valid VeiculoReqDTO dto) {
 		Usuario usuario = (Usuario) authentication.getPrincipal();
-		return veiculoService.criar(dto, usuario.getOficina());
+		VeiculoResDTO criado = veiculoService.criar(dto, usuario.getOficina());
+		return ResponseEntity.ok(criado);
+	}
+	
+	@PutMapping("/{idVeiculo}")
+	public ResponseEntity<VeiculoResDTO> atualizar(@ModelAttribute @Valid VeiculoReqDTO dto, @PathVariable(required=true) Long idVeiculo) {
+		VeiculoResDTO atualizado = veiculoService.atualizar(dto, idVeiculo);
+		return ResponseEntity.ok(atualizado);
 	}
 }

10e4241359078acb74c7dc57459977e98fcdf4c7
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-13 13:33:01 -0300

    Create VeiculoSpecification.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/specification/VeiculoSpecification.java
@@ -0,0 +1,42 @@
+package com.fooengineers.projetoAcVansV4.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.fooengineers.projetoAcVansV4.entity.Veiculo;
+
+import jakarta.persistence.criteria.Predicate;
+
+public class VeiculoSpecification {
+	public static Specification<Veiculo> filtroGeral(String termo, Integer idOficina){
+		return (root, query, cb) -> {
+			if(termo == null || termo.isBlank()) {
+				return cb.equal(root.get("oficina").get("id"), idOficina);
+			}
+			String like = "%" + termo.toLowerCase() + "%";
+			
+			Predicate porPlaca = cb.like(cb.lower(root.get("placa")), like);
+			Predicate porMarca = cb.like(cb.lower(root.get("marca")), like);
+			Predicate porModelo = cb.like(cb.lower(root.get("modelo")), like);
+			
+			Predicate porOficina = cb.equal(root.get("oficina").get("id"), idOficina);
+			
+			return cb.and(porOficina, cb.or(porPlaca, porMarca, porModelo));
+		};
+	}
+	public static Specification<Veiculo> filtrarPorCliente(String termo, Long idCliente){
+		return (root, query, cb) -> {
+			if(termo == null || termo.isBlank()) {
+				return cb.equal(root.get("oficina").get("id"), idCliente);
+			}
+			String like = "%" + termo.toLowerCase() + "%";
+			
+			Predicate porPlaca = cb.like(cb.lower(root.get("placa")), like);
+			Predicate porMarca = cb.like(cb.lower(root.get("marca")), like);
+			Predicate porModelo = cb.like(cb.lower(root.get("modelo")), like);
+			
+			Predicate porOficina = cb.equal(root.get("oficina").get("id"), idCliente);
+			
+			return cb.and(porOficina, cb.or(porPlaca, porMarca, porModelo));
+		};
+	}
+}

f7c3508b50a6ba19f960279dfaaa41c53277ce14
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 16:25:21 -0300

    Movido (Nova estrutura de pacotes)

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/repository/AuditoriaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/repository/AuditoriaRepository.java
@@ -1,10 +1,10 @@
-package com.fooengineers.projetoAcVansV4.repository;
+package com.fooengineers.projetoAcVansV4.auditoria.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.fooengineers.projetoAcVansV4.entity.Auditoria;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Auditoria;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 
 public interface AuditoriaRepository extends JpaRepository<Auditoria, Long> {

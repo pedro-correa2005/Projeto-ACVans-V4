1e39494edb4aa25d751f9d852f8aac892caf3caa
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 19:43:45 -0300

    Update ClienteService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/ClienteService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/ClienteService.java
@@ -1,10 +1,18 @@
 package com.fooengineers.projetoAcVansV4.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fooengineers.projetoAcVansV4.auditoria.dto.Detalhes;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Acao;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Entidade;
+import com.fooengineers.projetoAcVansV4.auditoria.service.AuditoriaService;
 import com.fooengineers.projetoAcVansV4.dto.ClienteReqDTO;
 import com.fooengineers.projetoAcVansV4.dto.ClienteResDTO;
 import com.fooengineers.projetoAcVansV4.entity.Cliente;
@@ -16,7 +24,11 @@ import com.fooengineers.projetoAcVansV4.specification.ClienteSpecification;
 @Service
 public class ClienteService {
 	@Autowired
-	ClienteRepository clienteRepository;
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private AuditoriaService auditoriaService;
+	
+	private final ObjectMapper objectMapper = new ObjectMapper();
 	
 	public Page<ClienteResDTO> listar(Oficina oficina, Pageable pageable){
 		return clienteRepository.findByOficina(oficina, pageable).map(ClienteResDTO::new);
@@ -43,9 +55,32 @@ public class ClienteService {
 
 	public ClienteResDTO atualizar(ClienteReqDTO dto, Long idCliente) {
 		Cliente cliente = clienteRepository.findById(idCliente).orElseThrow(() -> new ClienteNaoEncontradoExcepiton(idCliente));
+		
+		Map<String, Object> antes =
+				objectMapper.convertValue(
+						new ClienteResDTO(cliente),
+						new TypeReference<Map<String, Object>>() {}
+						);
+
 		cliente.setNome(dto.getNome());
 		cliente.setCelular(dto.getCelular());
+		
 		Cliente salvo = clienteRepository.save(cliente);
+		
+		Map<String, Object> depois =
+		        objectMapper.convertValue(
+		                new ClienteResDTO(salvo),
+		                new TypeReference<Map<String, Object>>() {}
+		        );
+		
+		Detalhes detalhes = new Detalhes(antes, depois);
+		
+		auditoriaService.registrar(
+				Acao.UPDATE,
+				Entidade.CLIENTE,
+				idCliente,
+				detalhes);
+		
 		return new ClienteResDTO(salvo);
 	}
 	public void deletar(Long idCliente) {

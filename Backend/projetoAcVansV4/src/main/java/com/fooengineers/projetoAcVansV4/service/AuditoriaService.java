e4776491fa9aed48b854493c5a20566430edbf53
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 13:28:41 -0300

    Update AuditoriaService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuditoriaService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/AuditoriaService.java
@@ -1,12 +1,19 @@
 package com.fooengineers.projetoAcVansV4.service;
 
+import java.sql.Timestamp;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.fooengineers.projetoAcVansV4.domain.Acao;
+import com.fooengineers.projetoAcVansV4.domain.Detalhes;
+import com.fooengineers.projetoAcVansV4.domain.Entidade;
 import com.fooengineers.projetoAcVansV4.dto.AuditoriaDTO;
+import com.fooengineers.projetoAcVansV4.entity.Auditoria;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
 import com.fooengineers.projetoAcVansV4.repository.AuditoriaRepository;
 
 @Service
@@ -16,5 +23,18 @@ public class AuditoriaService {
 	
 	public Page<AuditoriaDTO> consultar(Oficina oficina, Pageable pageable) {
 		return auditoriaRepository.findByOficinaOrderByTempoDesc(oficina, pageable).map(AuditoriaDTO::new);
+	}
+	
+	public void registrar(Acao acao, Entidade entidade, Long idRegistro, String enderecoIp, Detalhes detalhes, Usuario usuario, Oficina oficina) {
+		Auditoria auditoria = new Auditoria();
+		auditoria.setAcao(acao);
+		auditoria.setEntidade(entidade);
+		auditoria.setIdRegistro(idRegistro);
+		auditoria.setTempo(new Timestamp(System.currentTimeMillis()));
+		auditoria.setEnderecoIp(enderecoIp);
+		auditoria.setDetalhes(detalhes);
+		auditoria.setUsuario(usuario);
+		auditoria.setOficina(oficina);
+		auditoriaRepository.save(auditoria);
 	}
 }

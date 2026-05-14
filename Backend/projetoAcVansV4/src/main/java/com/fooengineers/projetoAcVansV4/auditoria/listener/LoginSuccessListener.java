7e1e02341bd8440ba99e6d439ed1d296915d3373
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 19:43:24 -0300

    Update LoginSuccessListener.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/listener/LoginSuccessListener.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/listener/LoginSuccessListener.java
@@ -4,15 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.fooengineers.projetoAcVansV4.auditoria.entity.Acao;
 import com.fooengineers.projetoAcVansV4.auditoria.service.AuditoriaService;
-import com.fooengineers.projetoAcVansV4.entity.Usuario;
-import com.fooengineers.projetoAcVansV4.util.IpUtil;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -23,21 +18,10 @@ public class LoginSuccessListener {
 	
 	@EventListener
 	public void onSuccess(AuthenticationSuccessEvent event) {
-		Usuario usuario = (Usuario) event.getAuthentication().getPrincipal();
-		
-		HttpServletRequest request = ((ServletRequestAttributes)
-				RequestContextHolder.getRequestAttributes())
-				.getRequest();
-		
-		String ip = IpUtil.getClientIp(request);
-		
 		auditoriaService.registrar(
 				Acao.LOGIN,
 				null,
 				null,
-				ip,
-				null, 
-				usuario,
-				usuario.getOficina());
+				null);
 	}
 }

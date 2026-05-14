c1d2c8dc7eb6397ed68bcf54858b1c65a5ced59b
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 01:42:51 -0300

    Update EtapaServicoRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/EtapaServicoRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/EtapaServicoRepository.java
@@ -3,9 +3,11 @@ package com.fooengineers.projetoAcVansV4.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 
 public interface EtapaServicoRepository extends JpaRepository<EtapaServico, Long> {
@@ -22,4 +24,5 @@ public interface EtapaServicoRepository extends JpaRepository<EtapaServico, Long
 	//Listagem por tipo de serviço
 	List<EtapaServico> findByTipoServico(TipoServico tipoServico);
 	
+	List<EtapaServico> findByOficina(Oficina oficina, Sort s);
 }

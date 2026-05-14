448baebdd083e2e809a34e702bb9c62cef402ddf
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 12:51:10 -0300

    Create AuditoriaController.java

---
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/AuditoriaController.java
@@ -0,0 +1,27 @@
+package com.fooengineers.projetoAcVansV4.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fooengineers.projetoAcVansV4.dto.AuditoriaDTO;
+import com.fooengineers.projetoAcVansV4.entity.Oficina;
+import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.service.AuditoriaService;
+
+@RestController
+@RequestMapping("/api/auditoria")
+public class AuditoriaController {
+	@Autowired
+	AuditoriaService auditoriaService;
+	
+	@GetMapping
+	public Page<AuditoriaDTO> auditar(Authentication authentication, Pageable pageable){
+		Oficina oficina = ((Usuario) authentication.getPrincipal()).getOficina();
+		return auditoriaService.consultar(oficina, pageable);
+	}
+}

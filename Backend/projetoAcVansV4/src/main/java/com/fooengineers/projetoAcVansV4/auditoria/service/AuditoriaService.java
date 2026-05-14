df7ccb750dd3d2e1d52d355cf36992070f83d691
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 19:43:33 -0300

    Update AuditoriaService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/service/AuditoriaService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/service/AuditoriaService.java
@@ -5,16 +5,23 @@ import java.sql.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.fooengineers.projetoAcVansV4.auditoria.dto.AuditoriaDTO;
 import com.fooengineers.projetoAcVansV4.auditoria.dto.Detalhes;
 import com.fooengineers.projetoAcVansV4.auditoria.entity.Acao;
 import com.fooengineers.projetoAcVansV4.auditoria.entity.Auditoria;
 import com.fooengineers.projetoAcVansV4.auditoria.entity.Entidade;
 import com.fooengineers.projetoAcVansV4.auditoria.repository.AuditoriaRepository;
-import com.fooengineers.projetoAcVansV4.dto.AuditoriaDTO;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
+import com.fooengineers.projetoAcVansV4.util.IpUtil;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class AuditoriaService {
@@ -25,16 +32,23 @@ public class AuditoriaService {
 		return auditoriaRepository.findByOficinaOrderByTempoDesc(oficina, pageable).map(AuditoriaDTO::new);
 	}
 	
-	public void registrar(Acao acao, Entidade entidade, Long idRegistro, String enderecoIp, Detalhes detalhes, Usuario usuario, Oficina oficina) {
+	public void registrar(Acao acao, Entidade entidade, Long idRegistro, Detalhes detalhes) {
 		Auditoria auditoria = new Auditoria();
 		auditoria.setAcao(acao);
 		auditoria.setEntidade(entidade);
 		auditoria.setIdRegistro(idRegistro);
 		auditoria.setTempo(new Timestamp(System.currentTimeMillis()));
-		auditoria.setEnderecoIp(enderecoIp);
+		
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		HttpServletRequest request = attr.getRequest();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Usuario usuario = (Usuario) authentication.getPrincipal();
+		
+		auditoria.setEnderecoIp(IpUtil.getClientIp(request));
 		auditoria.setDetalhes(detalhes);
 		auditoria.setUsuario(usuario);
-		auditoria.setOficina(oficina);
+		if(usuario != null) auditoria.setOficina(usuario.getOficina());
+		
 		auditoriaRepository.save(auditoria);
 	}
 }

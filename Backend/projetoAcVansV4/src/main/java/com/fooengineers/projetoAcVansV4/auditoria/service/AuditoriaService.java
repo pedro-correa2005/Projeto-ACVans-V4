26a30e6d53ba2476f9f7cc4cd5cdcf2946e8c461
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 16:26:04 -0300

    Movido (Nova estrutura de pacotes)

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/service/AuditoriaService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/auditoria/service/AuditoriaService.java
@@ -1,4 +1,4 @@
-package com.fooengineers.projetoAcVansV4.service;
+package com.fooengineers.projetoAcVansV4.auditoria.service;
 
 import java.sql.Timestamp;
 
@@ -7,14 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.fooengineers.projetoAcVansV4.domain.Acao;
-import com.fooengineers.projetoAcVansV4.domain.Detalhes;
-import com.fooengineers.projetoAcVansV4.domain.Entidade;
+import com.fooengineers.projetoAcVansV4.auditoria.dto.Detalhes;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Acao;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Auditoria;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Entidade;
+import com.fooengineers.projetoAcVansV4.auditoria.repository.AuditoriaRepository;
 import com.fooengineers.projetoAcVansV4.dto.AuditoriaDTO;
-import com.fooengineers.projetoAcVansV4.entity.Auditoria;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
 import com.fooengineers.projetoAcVansV4.entity.Usuario;
-import com.fooengineers.projetoAcVansV4.repository.AuditoriaRepository;
 
 @Service
 public class AuditoriaService {

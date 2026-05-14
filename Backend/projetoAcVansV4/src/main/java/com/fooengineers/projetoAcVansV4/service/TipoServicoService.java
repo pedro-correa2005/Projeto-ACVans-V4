173fb55e2f97b14611f097aab9d9836c0ee9bd63
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-14 20:16:38 -0300

    Update TipoServicoService.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/TipoServicoService.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/service/TipoServicoService.java
@@ -1,11 +1,18 @@
 package com.fooengineers.projetoAcVansV4.service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fooengineers.projetoAcVansV4.auditoria.dto.Detalhes;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Acao;
+import com.fooengineers.projetoAcVansV4.auditoria.entity.Entidade;
+import com.fooengineers.projetoAcVansV4.auditoria.service.AuditoriaService;
 import com.fooengineers.projetoAcVansV4.dto.TipoServicoReqDTO;
 import com.fooengineers.projetoAcVansV4.dto.TipoServicoResDTO;
 import com.fooengineers.projetoAcVansV4.entity.Oficina;
@@ -17,6 +24,10 @@ import com.fooengineers.projetoAcVansV4.repository.TipoServicoRepository;
 public class TipoServicoService {
 	@Autowired
 	TipoServicoRepository tipoServicoRepository;
+	@Autowired
+	private AuditoriaService auditoriaService;
+	
+	private final ObjectMapper objectMapper = new ObjectMapper();
 	
 	public List<TipoServicoResDTO> listar(Oficina oficina){
 		return tipoServicoRepository.findByOficina(oficina).stream()
@@ -34,8 +45,30 @@ public class TipoServicoService {
 	
 	public TipoServicoResDTO atualizar(TipoServicoReqDTO dto, Long idTipoServico) {
 		TipoServico tipo = tipoServicoRepository.findById(idTipoServico).orElseThrow(() -> new TipoServicoNaoEncontradoException(idTipoServico));
+		
+		Map<String, Object> antes =
+				objectMapper.convertValue(
+						new TipoServicoResDTO(tipo),
+						new TypeReference<Map<String, Object>>() {}
+						);
+
+		
 		tipo.setDescricao(dto.getDescricao());
 		TipoServico atualizado = tipoServicoRepository.save(tipo);
+		
+		Map<String, Object> depois =
+		        objectMapper.convertValue(
+		                new TipoServicoResDTO(atualizado),
+		                new TypeReference<Map<String, Object>>() {}
+		        );
+		Detalhes detalhes = new Detalhes(antes, depois);
+		
+		auditoriaService.registrar(
+				Acao.UPDATE,
+				Entidade.TIPO_SERVICO,
+				idTipoServico,
+				detalhes);
+		
 		return new TipoServicoResDTO(atualizado);
 	}
 	

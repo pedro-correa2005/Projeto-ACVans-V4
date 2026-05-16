eadaa37bdf78f81e0497cfb9212ecae02a386d3f
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-16 14:56:25 -0300

    Update Footer.jsx

--- Frontend/acvans-front/src/components/Footer.jsx
+++ Frontend/acvans-front/src/components/Footer.jsx
@@ -1,16 +1,15 @@
-function Footer(){
-    return (
-        <footer className="bg-gray-100 border-t mt-auto">
-            <div className="max-w-7x1 mx-auto px-6 py-4 text-center text-sm text-gray-600">
-                <p>
-                    © 2026 AC Vans - Todos os direitos reservados.
-                </p>
-                <p>
-                    Desenvolvido por FooEngineers
-                </p>
-            </div>
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer className="main-footer">
+      <p>
+        © 2026 AC Vans - Todos os direitos reservados.
+      </p>
+
+      <p>
+        Desenvolvido por FooEngineers
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;

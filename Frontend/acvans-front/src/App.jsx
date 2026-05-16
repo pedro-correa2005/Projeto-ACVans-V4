775df5d6a74a43d70b659fedce474e4b680ca87a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-16 17:42:17 -0300

    Update App.jsx

--- Frontend/acvans-front/src/App.jsx
+++ Frontend/acvans-front/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import Login from "./pages/Login"
 import Consulta from "./pages/Consulta"
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,8 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/consultar-servico" element={<Consulta />}></Route>
       </Routes>
+
+      <ToastContainer position="top-right autoClose={3000}"/>
     </BrowserRouter>
   );
 }

ac5bc3593db48b5592374dbc1bda308ad6370ef0
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-16 14:56:18 -0300

    Update App.jsx

--- Frontend/acvans-front/src/App.jsx
+++ Frontend/acvans-front/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
-
+import Login from "./pages/Login"
+import Consulta from "./pages/Consulta"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/consultar-servico" element={<Consulta />}></Route>
       </Routes>
     </BrowserRouter>
   );

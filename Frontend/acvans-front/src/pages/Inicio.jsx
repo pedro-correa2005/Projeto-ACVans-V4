ced7a8049de031d47101eaf08653721ab2d74fd4
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-16 14:56:09 -0300

    Update Inicio.jsx

--- Frontend/acvans-front/src/pages/Inicio.jsx
+++ Frontend/acvans-front/src/pages/Inicio.jsx
@@ -4,43 +4,57 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Inicio() {
-    return (
-        <div className="min-h-screen flex flex-col bg-gray-50">
-            <Header />
-            <main className="fex-1">
-                <div className="min-h-[80vh] flex items-center justify-center px-6">
-                    <div className="text-center">
-                        <h1 className="text-5x1 front-bold mb-8 text-gray-800">
-                            Bem-vindo ao Sistema
-                        </h1>
-                        <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                            <Link 
-                                to="/consultar-servico" 
-                                className="bg-sky-600 hover:bg-sky-700 text-white px-8 py-4 rounded-lg text-lg font-semibold transition">
-                                Consultar Serviço
-                            </Link>
-                            <Link
-                                to="/cadastros"
-                                className="
-                                bg-gray-700 
-                                hover:bg-gray-800 
-                                text-white 
-                                px-8 
-                                py-4 
-                                rounded-lg 
-                                text-lg 
-                                font-semibold 
-                                transition
-                                ">
-                                    Acessar Sistema
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </main>
-            <Footer />
+
+  return (
+    <div>
+
+      <Header />
+
+      <main>
+
+        <div
+          className="
+            min-h-screen
+            flex
+            justify-center
+            items-center
+          "
+        >
+
+          <div className="text-center">
+
+            <h1 className="text-5xl font-bold mb-8">
+              Bem-vindo ao Sistema
+            </h1>
+
+            <div className="flex gap-4 justify-center flex-wrap">
+
+              <Link
+                to="/consultar-servico"
+                className="btn btn-primary"
+              >
+                Consultar Serviço
+              </Link>
+
+              <Link
+                to="/cadastros"
+                className="btn btn-secondary"
+              >
+                Acessar Sistema
+              </Link>
+
+            </div>
+
+          </div>
+
         </div>
-    );
+
+      </main>
+
+      <Footer />
+
+    </div>
+  );
 }
 
 export default Inicio;

9e9e44dc493dc126f1cb7cbacbda9ab0d45f081d
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-17 15:53:35 -0300

    Update main.jsx

--- Frontend/acvans-front/src/main.jsx
+++ Frontend/acvans-front/src/main.jsx
@@ -9,8 +9,14 @@ import './styles/style.css'
 
 import App from './App.jsx'
 
+import {
+  AuthProvider
+} from './context/authContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )

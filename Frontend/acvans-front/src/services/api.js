9effd243d9c7dc7791ddf0d4e9cf29817c531975
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-17 23:27:04 -0300

    Update api.js

--- Frontend/acvans-front/src/services/api.js
+++ Frontend/acvans-front/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { getCookie } from "../utils/cookies";
+
 const api = axios.create({
     baseURL: "http://localhost:8080/api",
     withCredentials: true
@@ -7,7 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const csrfToken = sessionStorage.getItem("csrf_token");
+        const csrfToken = getCookie("csrf_token");
         if(csrfToken){
             config.headers[
                 "X-CSRF-TOKEN"
@@ -23,34 +25,40 @@ api.interceptors.request.use(
 );
 
 api.interceptors.response.use(
-    (response) => response,
+  (response) => response,
 
-    async (error) => {
-        const originalRequest = error.config;
+  async (error) => {
+    const originalRequest = error.config;
 
-        if(error.response?.status === 403 && !originalRequest._retry){
-            originalRequest._retry = true;
-
-            try{
-                const resposne = await api.post("/auth/refresh");
-
-                const novoCsrf = response.data.csrf_token;
-
-                if(novoCsrf){
-                    sessionStorage.setItem("csrf_token",novoCsrf);
-                }
-
-                return api(originalRequest);
-            } catch(refreshError){
-                sessionStorage.removeItem("csrf_token");
-
-                window.location.href="/login";
-
-                return Promise.reject(refreshError);
-            }
-        }
-        return Promise.reject(error);
+    if (originalRequest.url?.includes("/auth/refresh")) {
+      sessionStorage.removeItem("csrf_token");
+      return Promise.reject(error);
     }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+
+      originalRequest._retry = true;
+
+      try {
+        const response = await api.post("/auth/refresh");
+        const novoCsrf = response.data.csrfToken;
+        
+        if (novoCsrf) {
+          sessionStorage.setItem("csrf_token", novoCsrf);
+        }
+        return api(originalRequest);
+
+      } catch (refreshError) {
+        sessionStorage.removeItem(
+          "csrf_token"
+        );
+        return Promise.reject(
+          refreshError
+        );
+      }
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;

517bb19a840c3d896de36af888d3343e5d51d2d7
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-18 00:46:10 -0300

    Update App.jsx

--- Frontend/acvans-front/src/App.jsx
+++ Frontend/acvans-front/src/App.jsx
@@ -6,15 +6,17 @@ import RespostaConsulta from "./pages/RespostaConsulta"
 import Perfil from "./pages/Perfil";
 import { ToastContainer } from "react-toastify";
 import PrivateRoute from "./components/PrivateRoute";
+import TwoFactor from "./pages/TwoFactor";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/consultar-servico" element={<Consulta />}></Route>
-        <Route path="/consulta" element={<RespostaConsulta />}></Route>
-        <Route path="/perfil" element={<PrivateRoute><Perfil/></PrivateRoute>}> </Route>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/consultar-servico" element={<Consulta />}/>
+        <Route path="/consulta" element={<RespostaConsulta />}/>
+        <Route path="/perfil" element={<PrivateRoute><Perfil/></PrivateRoute>}/>
+        <Route path="/2fa" element={<TwoFactor />}/>
       </Routes>
 
       <ToastContainer position="top-right autoClose={300}"/>

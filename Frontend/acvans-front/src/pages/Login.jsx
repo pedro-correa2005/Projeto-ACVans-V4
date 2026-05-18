8781d660f316004ca0baa077fed31a8eaeeed79a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-17 23:27:13 -0300

    Create Login.jsx

---
+++ Frontend/acvans-front/src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import {
+  useAuth
+} from "../context/authContext";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const {
+    login,
+    authenticated
+  } = useAuth();
+  console.log(authenticated);
+
+  const [email, setEmail] = useState("");
+  
+  const [senha, setSenha] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  
+  if(authenticated){
+    return(
+      <Navigate to="/cadastros" />
+    );
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    try{
+      setLoading(true);
+      await login(email, senha);
+      toast.success("Login realizado com sucesso!");
+      navigate("/cadastros");
+    }catch(error){
+      console.error(error);
+      const mensagem = error.respones?.data?.message || "Erro ao realizar login";
+      toast.error(mensagem);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <main className="container mt-4">
+        <div className="form-wrapper">
+          <h1>Acesso ao Sistema</h1>        
+          <form className="form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+              </div>
+              <div className="form-group">
+                  <label htmlFor="password">Senha</label>
+                  <input type="password" id="password" name="password" value={senha} onChange={(e) => {setSenha(e.target.value)}}/>
+              </div>
+              <div className="form-actions">
+                  <button type="submit" className="btn btn-primary" disabled={loading}>{loading?"Entrando...":"Entrar"}</button>
+              </div>
+          </form>
+        </div>
+      </main>
+    <Footer />
+  </>
+  );
+}
+
+export default Login;

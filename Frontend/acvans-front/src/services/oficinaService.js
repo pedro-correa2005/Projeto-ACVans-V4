1b3767d05394fd80aaeeb83d2acc4fba9ab5d7fc
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-19 17:02:07 -0300

    Update oficinaService.js

--- Frontend/acvans-front/src/services/oficinaService.js
+++ Frontend/acvans-front/src/services/oficinaService.js
@@ -1,15 +1,16 @@
 import api from "./api";
 
-export async function listarOficinas({
-    termo = ""
-}){
-    const response = await api.get("/admin/oficinas", {params: {termo}});
+export async function listarOficinas(termo, pagina, tamanho = 10){
+    const response = await api.get("/admin/oficinas", {params: {termo, page: pagina, size: tamanho}});
     return response.data;
 }
 
-export async function criarOficina({
-    oficina
-}){
+export async function criarOficina(oficina){
     const response = await api.post("/admin/oficinas", oficina);
+    return response.data;
+}
+
+export async function atualizarOficina(id, oficina) {
+    const response = await api.put(`/admin/oficinas/${id}`, oficina);
     return response.data;
 }

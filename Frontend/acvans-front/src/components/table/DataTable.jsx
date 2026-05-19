9f8702ddda55acc362b1a22fea12c78d1c2a53fc
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-19 17:01:28 -0300

    Update DataTable.jsx

--- Frontend/acvans-front/src/components/table/DataTable.jsx
+++ Frontend/acvans-front/src/components/table/DataTable.jsx
@@ -1,69 +1,71 @@
 function DataTable({
-    data = [],
+    page,
     columns = [],
     actions
 }){
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-hover align-middle data-table">
-                <thead>
-                    <tr>
+        <div className="table-wrapper">
+            <div className="table-responsive">
+                <table className="table table-striped table-hover align-middle data-table">
+                    <thead>
+                        <tr>
+                            {
+                                columns.map(column => (
+                                    <th key={column.key}>
+                                        {column.label}
+                                    </th>
+                                ))
+                            }
+                            {
+                                actions && (
+                                    <th className="col-acoes">Ações</th>
+                                )
+                            }
+                        </tr>
+                    </thead>
+                    <tbody>
                         {
-                            columns.map(column => (
-                                <th key={column.key}>
-                                    {column.lable}
-                                </th>
-                            ))
-                        }
-                        {
-                            actions && (
-                                <th>Ações</th>
+                            page?.content?.length === 0 && (
+                                <tr>
+                                    <td colSpan={columns.length + 1} className="text-center">
+                                        Nenhum registro encontrado.
+                                    </td>
+                                </tr>
                             )
                         }
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        data.length === 0 && (
-                            <tr>
-                                <td colSpan={columns.length + 1} className="text-center">
-                                    Nenhum registro encontrado.
-                                </td>
-                            </tr>
-                        )
-                    }
-                    {
-                        data.map(item => (
-                            <tr key={item.id}>
-                                {
-                                    columns.map(column => (
-                                        <td key={column.key}>
-                                            {
-                                                column.render
-                                                ?
-                                                column.render(
-                                                    item[column.key],
-                                                    item
-                                                )
-                                                :item[column.key]
-                                            }
-                                        </td>
-                                    ))
-                                }
-                                {
-                                    actions && (
-                                        <td>
-                                            {
-                                                actions(item)
-                                            }
-                                        </td>
-                                    )
-                                }
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                        {
+                            page?.content?.map(item => (
+                                <tr key={item.id}>
+                                    {
+                                        columns.map(column => (
+                                            <td key={column.key}>
+                                                {
+                                                    column.render
+                                                    ?
+                                                    column.render(
+                                                        item[column.key],
+                                                        item
+                                                    )
+                                                    :item[column.key]
+                                                }
+                                            </td>
+                                        ))
+                                    }
+                                    {
+                                        actions && (
+                                            <td className="col-acoes">
+                                                {
+                                                    actions(item)
+                                                }
+                                            </td>
+                                        )
+                                    }
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }

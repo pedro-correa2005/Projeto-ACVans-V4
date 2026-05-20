ffe0cfe934878746e77cb894f580470844784e88
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-20 16:24:01 -0300

    Create clienteService.js

---
+++ Frontend/acvans-front/src/services/clienteService.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export async function listarClientes(termo, pagina, tamanho = 10, sort) {
+    const response = await api.get(`/clientes`, {params: {termo, page: pagina, size: tamanho, sort: sort}});
+    return response.data;
+}
+
+export async function criarCliente(cliente){
+    const response = await api.post(`/clientes`, cliente);
+    return response.data;
+}
+
+export async function atualizarCliente(id, cliente){
+    const response = await api.put(`/clientes/${id}`, cliente);
+    return response.data;
+}
+
+export async function deletarCliente(id){
+    const response = await api.delete(`/clientes/${id}`);
+    return response.data;
+}

d82defae12de7de698ac25056a3043021aa6539e
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-20 13:20:29 -0300

    Update usuarioService.js

--- Frontend/acvans-front/src/services/usuarioService.js
+++ Frontend/acvans-front/src/services/usuarioService.js
@@ -4,3 +4,23 @@ export async function listarRoles(){
     const response = await api.get("/admin/roles");
     return response.data;
 }
+
+export async function listarUsuarios(idOficina, termo, pagina, tamanho = 10, sort) {
+    const response = await api.get(`/admin/oficinas/${idOficina}/usuarios`);
+    return response.data;
+}
+
+export async function criarUsuario(usuario, idOficina){
+    const response = await api.post(`/admin/oficinas/${idOficina}/usuarios`, usuario);
+    return response.data;
+}
+
+export async function atualizarRoles(id, roles){
+    const response = await api.put(`/admin/usuarios/${id}/atualizar-roles`, roles);
+    return response.data;
+}
+
+export async function deletarUsuario(id){
+    const response = await api.delete(`/admin/usuario/${id}`);
+    return response.data;
+}

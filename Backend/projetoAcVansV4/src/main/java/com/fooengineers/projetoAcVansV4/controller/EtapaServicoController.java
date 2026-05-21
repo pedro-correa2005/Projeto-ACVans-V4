e075d976d17b85922c7ec6739fe0e22665e3b1fc
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-21 14:09:08 -0300

    model attribute -> request body

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/EtapaServicoController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/EtapaServicoController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,19 +37,19 @@ public class EtapaServicoController {
 	
 	@PostMapping("/tipos-servico/{idTipoServico}/etapas-servico")
 	@Auditavel(acao = Acao.CREATE, entidade = Entidade.ETAPA_SERVICO)
-	public ResponseEntity<EtapaServicoResDTO> criar(@ModelAttribute @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idTipoServico) {
+	public ResponseEntity<EtapaServicoResDTO> criar(@RequestBody @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idTipoServico) {
 		EtapaServicoResDTO criado = etapaServicoService.criar(dto, idTipoServico);
 		return ResponseEntity.ok().body(criado);
 	}
 	
 	@PutMapping("/etapas-servico/{idEtapaServico}")
-	public ResponseEntity<EtapaServicoResDTO >atualizar(@ModelAttribute @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idEtapaServico) {
+	public ResponseEntity<EtapaServicoResDTO >atualizar(@RequestBody @Valid EtapaServicoReqDTO dto, @PathVariable(required=true) Long idEtapaServico) {
 		EtapaServicoResDTO atualizado = etapaServicoService.atualizar(dto, idEtapaServico);
 		return ResponseEntity.ok().body(atualizado);
 	}
 	
 	@PatchMapping("/etapas-servico/{idEtapaServico}/atualizar-ordem")
-	public ResponseEntity<EtapaServicoResDTO> atualizarOrdem(@ModelAttribute @Valid OrdemDTO dto, @PathVariable Long idEtapaServico){
+	public ResponseEntity<EtapaServicoResDTO> atualizarOrdem(@RequestBody @Valid OrdemDTO dto, @PathVariable Long idEtapaServico){
 		EtapaServicoResDTO atualizado = etapaServicoService.atualizarOrdem(dto, idEtapaServico);
 		return ResponseEntity.ok().body(atualizado);
 	}
--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/TipoServicoController.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/controller/TipoServicoController.java
@@ -7,10 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,14 +38,14 @@ public class TipoServicoController {
 	
 	@PostMapping
 	@Auditavel(acao = Acao.CREATE, entidade = Entidade.TIPO_SERVICO)
-	public ResponseEntity<TipoServicoResDTO> criar(@ModelAttribute @Valid TipoServicoReqDTO dto, Authentication authentication) {
+	public ResponseEntity<TipoServicoResDTO> criar(@RequestBody @Valid TipoServicoReqDTO dto, Authentication authentication) {
 		Usuario usuario = (Usuario) authentication.getPrincipal();
 		TipoServicoResDTO criado = tipoServicoService.criar(dto, usuario.getOficina());
 		return ResponseEntity.ok().body(criado);
 	}
 	
 	@PutMapping("/{idTipoServico}")
-	public ResponseEntity<TipoServicoResDTO> atualizar(@ModelAttribute @Valid TipoServicoReqDTO dto, @PathVariable(required=true) Long idTipoServico) {
+	public ResponseEntity<TipoServicoResDTO> atualizar(@RequestBody @Valid TipoServicoReqDTO dto, @PathVariable(required=true) Long idTipoServico) {
 		TipoServicoResDTO atualizado = tipoServicoService.atualizar(dto, idTipoServico);
 		return ResponseEntity.ok().body(atualizado);
 	}

e196a27207ab9601a9ac985b7794bb7a97d4029b
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-20 22:44:55 -0300

    Create veiculoService.js

---
+++ Frontend/acvans-front/src/services/veiculoService.js
@@ -0,0 +1,21 @@
+import api from "./api";
+
+export async function listarVeiculos(termo, pagina, tamanho = 10, sort){
+    const response = await api.get(`/veiculos`, {params: {termo, page: pagina, size: tamanho, sort: sort}});
+    return response.data;
+}
+
+export async function criarVeiculo(veiculo) {
+    const response = await api.post(`/veiculos`, veiculo);
+    return response.data;
+}
+
+export async function atualizarVeiculo(id, veiculo){
+    const response = await api.put(`/veiculos/${id}`, veiculos);
+    return response.data;
+}
+
+export async function deletarVeiculo(id){
+    const response = await api.delete(`/veiculos/${id}`);
+    return response.data;
+}

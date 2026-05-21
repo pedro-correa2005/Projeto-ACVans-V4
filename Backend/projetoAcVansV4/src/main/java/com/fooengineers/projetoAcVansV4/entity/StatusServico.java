ef63ee4dcd34b4402cc150b6fe36c9588cd4fec6
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-21 15:16:07 -0300

    StatusServico.descricao deve ser unique

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/StatusServico.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/entity/StatusServico.java
@@ -21,6 +21,6 @@ public class StatusServico {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String descricao;
 }

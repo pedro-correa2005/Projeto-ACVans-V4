de6cb782f498dcbe5b0e107190fbe6ab037ef98a
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-21 15:22:23 -0300

    Create servicoService.js

---
+++ Frontend/acvans-front/src/services/servicoService.js
@@ -0,0 +1,22 @@
+import api from "./api";
+
+export async function listarServicos(termo, pagian, tamnho = 10, sort){
+    const response = await api.get("/servicos", {params: {termo, page: pagian, size: tamanho, sort: sort}});
+    return response.data;
+}
+
+export async function criarServico(servico) {
+    const response = await api.post("/servicos", servico);
+    return response.data;
+}
+
+export async function atualizarServico(id, servico) {
+    const response = await api.put(`/servicos/${id}`, servico);
+    return response.data;
+}
+
+
+export async function deletarServico(id) {
+    const response = await api.delete(`/servicos/${id}`);
+    return response.data;
+}

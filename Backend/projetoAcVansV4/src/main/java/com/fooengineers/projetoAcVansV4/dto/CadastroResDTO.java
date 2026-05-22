a858e90ae33add24c6da5bdf04f809a2b3edbd46
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-21 21:55:51 -0300

    Update CadastroResDTO.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/CadastroResDTO.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/CadastroResDTO.java
@@ -2,10 +2,8 @@ package com.fooengineers.projetoAcVansV4.dto;
 
 import java.sql.Timestamp;
 
-import com.fooengineers.projetoAcVansV4.entity.Cliente;
 import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.Servico;
-import com.fooengineers.projetoAcVansV4.entity.TipoServico;
 import com.fooengineers.projetoAcVansV4.entity.Veiculo;
 
 import lombok.AllArgsConstructor;
@@ -18,11 +16,10 @@ import lombok.NoArgsConstructor;
 public class CadastroResDTO {
 	private long idServico;
 	private String tokenConsulta;
-	private String veiculo;
-	private String descricaoTipoServico;
-	private String nomeCliente;
-	private String celularCliente;
-	private String receberNotificacao;
+	private VeiculoResDTO veiculo;
+	private TipoServicoResDTO tipo;
+	private boolean receberNotificacao;
+	private StatusServicoDTO status;
 	private Timestamp dataInicio;
 	private String etapaTitulo;
 	private Timestamp dataFim;
@@ -30,18 +27,23 @@ public class CadastroResDTO {
 	//Constructor a partir de entity.Servico
 	public CadastroResDTO(Servico servico) {
 		this.idServico = servico.getId();
+		
 		this.tokenConsulta = servico.getTokenConsulta();
+		
 		Veiculo veiculo = servico.getVeiculo();
-		this.veiculo = veiculo.getPlaca() + " " + veiculo.getMarca() + " " + veiculo.getModelo();
-		TipoServico tipo = servico.getTipoServico();
-		this.descricaoTipoServico = tipo.getDescricao();
-		Cliente cliente = veiculo.getCliente();
-		this.nomeCliente = cliente.getNome();
-		this.celularCliente = cliente.getCelular();
-		this.receberNotificacao = servico.getReceberNotificacao()?"Sim":"Não";
+		this.veiculo = new VeiculoResDTO(veiculo);
+		
+		this.tipo = new TipoServicoResDTO(servico.getTipoServico());
+		
+		this.receberNotificacao = servico.getReceberNotificacao();
+		
+		this.status = new StatusServicoDTO(servico.getStatusServico());
+		
 		this.dataInicio = servico.getDataInicio();
+		
 		EtapaServico etapa = servico.getEtapaServico();
 		this.etapaTitulo = etapa != null? etapa.getTitulo():null;
+		
 		this.dataFim = servico.getDataFim();
 	}
 }

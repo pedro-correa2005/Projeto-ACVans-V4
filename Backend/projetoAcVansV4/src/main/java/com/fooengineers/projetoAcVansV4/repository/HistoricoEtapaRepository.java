a65d820eaa47661fc504869abf606c4ae175f30e
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-22 12:47:47 -0300

    Update HistoricoEtapaRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
@@ -1,25 +1,13 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.HistoricoEtapa;
-import com.fooengineers.projetoAcVansV4.entity.Oficina;
-import com.fooengineers.projetoAcVansV4.entity.Servico;
 
-public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long>, JpaSpecificationExecutor<HistoricoEtapa> {
-	//Filtrar por servico, etapa e tenant
-	Optional<HistoricoEtapa> findByServicoAndEtapaServicoAndOficina(
-			Servico servico,
-			EtapaServico etapaServico,
-			Oficina oficina
-	);
-	List<HistoricoEtapa> findByOficina(Oficina oficina);
-	
+public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long>, JpaSpecificationExecutor<HistoricoEtapa> {	
 	List<HistoricoEtapa> findAll(Specification<HistoricoEtapa> specification);
 }

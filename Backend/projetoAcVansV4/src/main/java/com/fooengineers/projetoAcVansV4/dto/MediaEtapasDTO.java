3801543d74998e2914394ebe5e3fa7ce4de06eba
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-22 12:47:42 -0300

    Update MediaEtapasDTO.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/MediaEtapasDTO.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/dto/MediaEtapasDTO.java
@@ -1,14 +1,22 @@
 package com.fooengineers.projetoAcVansV4.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class MediaEtapasDTO {
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Etapa{		
+		protected String etapaTitulo;
+		protected float mediaMinutos;
+	}
+	
+	private List<Etapa> etapas;
 	private String tipoServicoDescricao;
-	private String etapaTitulo;
-	private float mediaMinutos;
 }

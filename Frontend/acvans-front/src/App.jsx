971802065039e12d0fbf6f2374afeca0be60b6b0
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-23 03:03:52 -0300

    Update App.jsx

--- Frontend/acvans-front/src/App.jsx
+++ Frontend/acvans-front/src/App.jsx
@@ -15,7 +15,8 @@ import Usuarios from "./pages/admin/Usuarios";
 import Cadastros from "./pages/Cadastros";
 import Clientes from "./pages/Clientes"
 import DetalhesCliente from "./pages/DetalhesCliente";
-import Veiculos from "./pages/Veiculos.jsx"
+import Veiculos from "./pages/Veiculos"
+import DetalhesVeiculo from "./pages/DetalhesVeiculo";
 import Relatorios from "./pages/gerente/Relatorios";
 import ConfigurarServicos from "./pages/gerente/ConfigurarServicos";
 import Auditoria from "./pages/gerente/Auditoria";
@@ -39,6 +40,7 @@ function App() {
         <Route path="/clientes" element={<PrivateRoute roles={["FUNCIONARIO", "GERENTE"]}><Clientes/></PrivateRoute>}/>
         {<Route path="/clientes/:idCliente" element={<PrivateRoute roles={["FUNCIONARIO", "GERENTE"]}><DetalhesCliente/></PrivateRoute>}/>}
         <Route path="/veiculos" element={<PrivateRoute roles={["FUNCIONARIO", "GERENTE"]}><Veiculos/></PrivateRoute>}/>
+        {<Route path="/veiculos/:idVeiculo" element={<PrivateRoute roles={["FUNCIONARIO", "GERENTE"]}><DetalhesVeiculo/></PrivateRoute>}/>}
         <Route path="/configurar-servicos" element={<PrivateRoute roles={["GERENTE"]}><ConfigurarServicos/></PrivateRoute>}/>
         <Route path="/relatorios" element={<PrivateRoute roles={["GERENTE"]}><Relatorios/></PrivateRoute>}/>
         <Route path="/auditoria" element={<PrivateRoute roles={["GERENTE"]}><Auditoria/></PrivateRoute>}/>

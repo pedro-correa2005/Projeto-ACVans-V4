9d8a689fbe49d50624fa843e2ce08c34b73bf675
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-23 01:43:06 -0300

    Update clienteService.js

--- Frontend/acvans-front/src/services/clienteService.js
+++ Frontend/acvans-front/src/services/clienteService.js
@@ -1,5 +1,10 @@
 import api from "./api";
 
+export async function buscarCliente(id){
+    const response = await api.get(`/clientes/${id}`);
+    return response.data;
+}
+
 export async function listarClientes(termo, pagina, tamanho = 10, sort) {
     const response = await api.get(`/clientes`, {params: {termo, page: pagina, size: tamanho, sort: sort}});
     return response.data;
@@ -17,5 +22,10 @@ export async function atualizarCliente(id, cliente){
 
 export async function deletarCliente(id){
     const response = await api.delete(`/clientes/${id}`);
+    return response.data;
+}
+
+export async function listarVeiculoPorCliente(id, termo, pagina, tamanho = 10, sort) {
+    const response = await api.get(`/clientes/${id}/veiculos`, {params: {termo, page: pagina, size: tamanho, sort: sort}});
     return response.data;
 }

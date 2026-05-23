bb2a206d36431e9fef001e254f310cb9e40fb45f
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-23 17:05:39 -0300

    Update HistoricoEtapaRepository.java

--- Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
+++ Backend/projetoAcVansV4/src/main/java/com/fooengineers/projetoAcVansV4/repository/HistoricoEtapaRepository.java
@@ -1,13 +1,17 @@
 package com.fooengineers.projetoAcVansV4.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import com.fooengineers.projetoAcVansV4.entity.EtapaServico;
 import com.fooengineers.projetoAcVansV4.entity.HistoricoEtapa;
+import com.fooengineers.projetoAcVansV4.entity.Servico;
 
-public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long>, JpaSpecificationExecutor<HistoricoEtapa> {	
+public interface HistoricoEtapaRepository extends JpaRepository<HistoricoEtapa, Long>, JpaSpecificationExecutor<HistoricoEtapa> {
+	Optional<HistoricoEtapa> findByServicoAndEtapaServico(Servico servico, EtapaServico etapa);
 	List<HistoricoEtapa> findAll(Specification<HistoricoEtapa> specification);
 }

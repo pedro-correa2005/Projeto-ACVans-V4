cd26ed9e06e95c6f8e83d9123cfe32184c864f55
pedro-correa2005 <pedrocorrea2005@outlook.com>
2026-05-23 17:06:18 -0300

    Update StatusTracker.jsx

--- Frontend/acvans-front/src/components/StatusTracker.jsx
+++ Frontend/acvans-front/src/components/StatusTracker.jsx
@@ -1,8 +1,10 @@
 function StatusTracker({
     etapas,
-    etapaAtualId
+    etapaAtualId,
+    finalizado=false
 }) {
     function etapaConcluida(etapa){
+        if(finalizado) return true;
         const atual = etapas.find(
             e => e.id === etapaAtualId
         );
@@ -46,6 +48,14 @@ function StatusTracker({
             </div>
 
             ))}
+
+            <div className={finalizado ? "step completed" : "step"}>
+                <div className="step-circle"></div>
+                <div>
+                    <div className="step-title">Finalizado</div>
+                    <div className="step-desc">Seu serviço já foi finalizado</div>
+                </div>
+            </div>
 
         </section>
     );
